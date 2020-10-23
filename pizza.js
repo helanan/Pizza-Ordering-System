1179431d98df95fc6c949560034b481632c4682f
Helana Belcher <helanabelcher@Helanas-MacBook-Pro-2.local>
2020-10-22 23:30:09 -0400

    reconfigured bootstrap cdns to html files, added bootstrap form to html for pizza topping picking, added modal pop up, added new customer object to javascript, added image and image folder for card display

--- pizza.js
+++ pizza.js
@@ -3,7 +3,7 @@ var Order = function() {
 	this.beverage = null;
 }
 
-//root object in a prototype chain
+//root pizza object in a prototype chain
 var Pizza = function() {
 	this.toppings = null;
 	this.crustThickness = 1;
@@ -27,39 +27,15 @@ var DeepDish = function(toppingsArr) {
 DeepDish.prototype = new Pizza();
 
 var TraditionalHandTossed = function() {
-}
-var Order = function() {
-	this.pizza = null;
-	this.beverage = null;
-}
-
-//root object in a prototype chain
-var Pizza = function() {
-	this.toppings = null;
-	this.crustThickness = 1;
-};
-
-Pizza.prototype.setCrustThickness = function(thickness) {
-	this.crustThickness = this.crustThickness * thickness
-}
-
-Pizza.prototype.setToppings = function(topping) {
-	this.toppings = this.toppings || [];
-	this.toppings.push(topping);
-}
-
-var DeepDish = function(toppingsArr) {
-	this.description = "Chewy and greasy, but so filling!";
+	this.description = "Spun by hand!";
 	this.setToppings.call(this, toppingsArr);
-	this.crust = this.setCrustThickness(3);
+	this.crust = this.setCrustThickness(1);
 }
 
-DeepDish.prototype = new Pizza();
-
-var TraditionalHandTossed = function() {
 var Order = function() {
 	this.pizza = null;
 	this.beverage = null;
+	this.customer = null;
 }
 
 //root object in a prototype chain
@@ -120,34 +96,46 @@ BananaPepper.prototype.makeBananaPepper = function () {
 	this.isBananaPepper = false;
 }
 
-// root function
+// root beverage function
 var Beverage = function() {
 	this.hasIce = true;
 };
 
 var Soda = function() {
+	this.name = 'Coke';
 
 };
 Soda.prototype = new Beverage();
 
+var Customer = function() {
+	this.first_name = 'Helana';
+	this.last_name = 'Belcher';
+	this.card_number = '01234567'
+};
+
+
 // a new order
 var order01 = new Order();
+
 var spicyPepp = new Pepperoni();
 spicyPepp.makeSpicy();
-console.log ("ourPepp", spicyPepp );
+
 var bananaPepp = new BananaPepper();
 
 var order01Toppings = [bananaPepp, spicyPepp];
-console.log("order one is ready!", order01Toppings);
 
-var order01Pizza = new DeepDish(...order01Toppings);
+
+var order01Pizza = new DeepDish(order01Toppings);
 console.log("Pizza", order01Pizza);
 order01Pizza.setCrustThickness(3);
 
 var order01Bev = new Soda();
 
+var  order01Customer = new Customer;
+
 order01.pizza = order01Pizza;
 order01.beverage = order01Bev;
+order01.customer  = order01Customer;
 
 console.log("order01", order01);
 
@@ -162,6 +150,44 @@ document.getElementById("add-ons").innerHTML = "<li class='list-inline-item'> Ic
 document.getElementById("add-ons").innerHTML = "<li class='list-inline-item'> Ice <i class='fas fa-dice-d6'></i> : " + "<i class='far fa-times-circle'></i>" + "</li>";
 }
 
+//order complete function
+
+//append toppings
+// $("button").click(function(){
+//   $("p").append("<b>Appended text</b>");
+// });
+
+// WIP:
+//document.getElementById("exampleFormControlSelect2")
+var no_topping = document.getElementById("option-one").innerHTML;
+
+console.log(no_topping);
 
 
-document.getElementById("orders").innerHTML = "<li> Description: " + order01.pizza.description.toString() + "</li>";
+sel = document.getElementsByClassName("topping_options");
+console.log("sel", sel);
+
+function getSelectedToppings(sel) {
+    var opt;
+    for ( var i = 0, len = sel.length; i < len; i++ ) {
+        opt = sel[i];
+      console.log(opt);
+        if ( opt.checked === true ) {
+            //break;
+          console.log(opt.checked);
+        }
+    }
+    return opt;
+}
+
+// get selected option in sel (reference obtained above)
+var opt = getSelectedToppings(sel);
+
+// //display its value and text
+// console.log( opt.innerHTML );
+// console.log( opt.text );
+
+ 
+function orderComplete(opt) {
+  document.getElementById("order-conf").innerHTML = opt.text;
+                        };
